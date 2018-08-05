2049a283b34389938f13936a503068093c9c70c2
safecoin miner wallet <mbj1231@live.dk>
2018-08-05 08:32:21 +0200

    Initial commit

---
+++ AC/AppCache/V2ZT6PHH/6/globalize.culture.da-dk[1].js
@@ -0,0 +1,36 @@
+/*! Note:  Microsoft Corporation is not the original author of these script files. Microsoft obtained
+the original file from http://github.com/jquery/globalize under the license that is referred to below. That
+license and the other notices below are provided for informational purposes only and are not
+the license terms under which Microsoft distributes the files.  Microsoft grants you the right
+to use the files for the sole purpose of using the files in conjunction with the Microsoft
+product with which it was distributed subject to that product's End User License Agreement.
+Unless applicable law gives you more rights, Microsoft reserves all other rights to the files
+not expressly granted by Microsoft, whether by implication, estoppel or otherwise.
+*/
+/*!
+* Globalize Culture da-DK
+*
+* http://github.com/jquery/globalize
+*
+* Copyright Software Freedom Conservancy, Inc.
+* ------------------------------------------------------------------------------------
+* MIT License
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* ------------------------------------------------------------------------------------
+*/
+(function(n){var t;t=typeof require!="undefined"&&typeof exports!="undefined"&&typeof module!="undefined"?require("globalize"):n.Globalize;t.addCultureInfo("da-DK","default",{name:"da-DK",englishName:"Danish (Denmark)",nativeName:"dansk (Danmark)",language:"da",numberFormat:{",":".",".":",",negativeInfinity:"-INF",positiveInfinity:"INF",percent:{",":".",".":","},currency:{pattern:["$ -n","$ n"],",":".",".":",",symbol:"kr."}},calendars:{standard:{"/":"-",firstDay:1,days:{names:["s\u00f8ndag","mandag","tirsdag","onsdag","torsdag","fredag","l\u00f8rdag"],namesAbbr:["s\u00f8","ma","ti","on","to","fr","l\u00f8"],namesShort:["s\u00f8","ma","ti","on","to","fr","l\u00f8"]},months:{names:["januar","februar","marts","april","maj","juni","juli","august","september","oktober","november","december",""],namesAbbr:["jan","feb","mar","apr","maj","jun","jul","aug","sep","okt","nov","dec",""]},AM:null,PM:null,patterns:{d:"dd-MM-yyyy",D:"d. MMMM yyyy",t:"HH:mm",T:"HH:mm:ss",f:"d. MMMM yyyy HH:mm",F:"d. MMMM yyyy HH:mm:ss",M:"d. MMMM",Y:"MMMM yyyy"}}},people:{headerDisplayValues:["#","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","\u00e6","\u00f8","\u00e5","*"],headerSeekToValues:["","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","\u00e6","\u00f8","\u00e5","\u03b1"]}})})(this)
